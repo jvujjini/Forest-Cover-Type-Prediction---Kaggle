8208e5a91e6435347bdfec84b5f2308a5c694796
Jagan Vujjini <jagan120@gmail.com>
2014-06-10 18:47:28 -0400

    Simple kNN

---
+++ load_and_predict.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+train_file = '/home/jvujjini/Kaggle/ForestCoverTypePrediction/train.csv'
+test_file = '/home/jvujjini/Kaggle/ForestCoverTypePrediction/test.csv'
+
+train_data = np.loadtxt(train_file, np.float32, delimiter=',')
+test_data = np.loadtxt(test_file, np.float32, delimiter=',')
+
+#training_data, training_label, test_data, test_label = train_data[:15000,:-1], data[:15000,-1], data[15000:,:-1], data[15000:,-1]
+
+train_X = train_data[:,:-1]
+train_y = train_data[:,-1]
+test_X = test_data
+
+knn = KNeighborsClassifier()
+print "Started Training..."
+knn.fit(train_X, train_y)
+print "Started Predicting..."
+predict_label = knn.predict(test_X)
+
+output_file = '/home/jvujjini/Kaggle/ForestCoverTypePrediction/output.csv'
+
+with open(output_file, 'w') as thefile:
+    print "File Opened..."
+    for item in predict_label:
+        thefile.write("%s\n" % item)
+
+print "Success!"
